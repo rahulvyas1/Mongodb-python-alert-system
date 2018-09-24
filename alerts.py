e79aae67bbb9eb830836c996d45a57eeca0a13c8
Rahul Vyas <rahulvyas@Rahuls-MacBook-Pro.local>
2018-09-24 22:56:22 +0530

    fixed URL of cluster

--- alerts.py
+++ alerts.py
@@ -1,10 +1,9 @@
 import pymongo
 
 from pymongo import MongoClient
-client = MongoClient()
-
-client = MongoClient('mongodb://localhost:27017')
-
-db = client.pymongo_test
-
-db = client['pymongo_test']
+# pprint library is used to make the output look more pretty
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+client = MongoClient("mongodb+srv://kay:7^303corA7Md@cluster0.mongodb.net/test")
+db=client.test
+# Issue the serverStatus command and print the results
+console.log(db)
