1313dc82f5dc5e9f4cb27a1de65d916ec8cf93b8
Rahul Vyas <rahulvyas@Rahuls-MacBook-Pro.local>
2018-09-24 22:57:26 +0530

    fixed print

--- alerts.py
+++ alerts.py
@@ -1,9 +1,7 @@
 import pymongo
 
-from pymongo import MongoClient
-# pprint library is used to make the output look more pretty
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 client = MongoClient("mongodb+srv://kay:7^303corA7Md@cluster0.mongodb.net/test")
 db=client.test
 # Issue the serverStatus command and print the results
-console.log(db)
+print(db)
